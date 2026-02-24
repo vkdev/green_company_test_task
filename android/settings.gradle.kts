5bad8419b0b0c8d5634d2ac179c5f20b22d3f733
vkdev <vk@vkdev.pro>
2026-02-24 18:42:04 +0300

    base module

---
+++ android/settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "GreenTest"
+include(":app")
+include(":HashMaker")
+include(":features:feature-list-compose")
+include(":repository:repository-impl")
+include(":repository:repository-api")
