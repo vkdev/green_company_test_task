52f82b04250409076992c7c3fcc7bf9523960cfb
vkdev <vk@vkdev.pro>
2026-03-03 18:51:40 +0300

    fimens fixed

--- android/ui-common/src/main/java/ru/vkdev/greentest/ui_common/dimen/DimensList.kt
+++ android/ui-common/src/main/java/ru/vkdev/greentest/ui_common/dimen/DimensList.kt
@@ -1,12 +1,11 @@
 package ru.vkdev.greentest.ui_common.dimen
 
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 
 object DimensList {
-    val contentPadding @Composable get() = 10.dp
-    val verticalSpacing @Composable get() = 15.dp
-    val itemCardElevation @Composable get() = 6.dp
-    val innerPaddingVertical @Composable get() = 5.dp
-    val innerPaddingHorizontal @Composable get() = 8.dp
+    val contentPadding = 10.dp
+    val verticalSpacing = 15.dp
+    val itemCardElevation = 6.dp
+    val innerPaddingVertical = 5.dp
+    val innerPaddingHorizontal = 8.dp
 }
--- android/ui-common/src/main/java/ru/vkdev/greentest/ui_common/dimen/DimensScreen.kt
+++ android/ui-common/src/main/java/ru/vkdev/greentest/ui_common/dimen/DimensScreen.kt
@@ -1,9 +1,8 @@
 package ru.vkdev.greentest.ui_common.dimen
 
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 
 object DimensScreen {
-    val paddingHorizontal @Composable get() = 16.dp
-    val paddingVertical @Composable get() = 8.dp
+    val paddingHorizontal = 16.dp
+    val paddingVertical = 8.dp
 }
